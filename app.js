b76fdbce81b4bf12e2573283e748d2562f2530e5
andirea19 <ungerer.andrea@gmail.com>
2024-03-21 15:15:47 +0100

    removed typo

--- app.js
+++ app.js
@@ -1,28 +1,28 @@
-const computerChoiceDisplay = document.getElementById('computer-choice')
-const playerChoiceDisplay = document.getElementById('player-choice')
-const resultDisplay = document.getElementById('result')
-const possibleChoices = document.querySelectorAll('button')
-let playerChoice
+const computerChoiceDisplay = document.getElementById('computer-choice');
+const playerChoiceDisplay = document.getElementById('player-choice');
+const resultDisplay = document.getElementById('result');
+const possibleChoices = document.querySelectorAll('button');
+let playerChoice;
 
-possibleChoices.forEach(possibleChoice => possibleChoice.addEventlistener('click', (e) => {
-    playerChoice =e.targetid
-    playerChoiceDisplay.innerHTML = playerChoice
-    generateComputerChoice()
+possibleChoices.forEach(possibleChoice => possibleChoice.addEventListener('click', (e) => {
+    playerChoice = e.target.id;
+    playerChoiceDisplay.innerHTML = playerChoice;
+    generateComputerChoice();
+}));
 
-}))
+function generateComputerChoice() {
+    const randomNumber = Math.floor(Math.random() * 3);
+    console.log(randomNumber);
 
-function generateComputerChoice(){
-    const randomNumber = Math.floor(Math.random() * 3)
-    console.log(randomNumber)
-
-    if (randomNumber == 1) {
-        generateComputerChoice = "Schere"
+    let computerChoice;
+    if (randomNumber === 0) {
+        computerChoice = "Schere";
     }
-    if (randomNumber == 2) {
-        generateComputerChoice = "Stein"
+    if (randomNumber === 1) {
+        computerChoice = "Stein";
     }
-    if (randomNumber == 3) {
-        generateComputerChoice = "Papier"
+    if (randomNumber === 2) {
+        computerChoice = "Papier";
     }
 
     computerChoiceDisplay.innerHTML = computerChoice;
